a8ffe40e73531315f27bcdd7e2d950d13e95dcbe
*Harsh-Kathare* <harshk1527@gmail.com>
2025-07-11 22:35:36 +0530

    Adding notes of lecture 1

---
+++ Lec1.java
@@ -0,0 +1,16 @@
+public class Lec1 {
+
+    public static void main(String[] args){
+
+        //This is my first java program
+
+        /*
+        This can be used
+        to create multiple line comment
+         */
+
+        System.out.println("I Like JAVA");//println can be used to create a new line
+        System.out.print("Its really good\n");// \n can also be used for new line
+        System.out.print("I am learning JAVA");//sout and then tab can give directly (System.out.println();
+    }
+}
